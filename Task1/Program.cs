c77297abea76e4e425df2d66c8330089d5523886
Влад Рязанцев <picachu361raichu@gmail.com>
2023-08-25 19:42:30 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static string[] SplitText(string text)
+        {
+            string[] split = text.Split(' ');
+            return split;
+        }
+
+        static void PrintText (string[] text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+
+            string userText = Console.ReadLine();
+            string[] words = SplitText(userText);
+
+            PrintText(words);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void SplitText(string text)
+        {
+            string[] split = text.Split(' ');
+            ReverseText(split);
+        }
+
+        static void ReverseText(string[] text)
+        {
+            string reverseWords = "";
+
+            for (int i = text.Length -1; i >= 0; i--)
+            {
+                reverseWords += text[i] + "\n"; 
+
+            }
+            Console.WriteLine(reverseWords);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+
+            string userText = Console.ReadLine();
+            SplitText(userText);
+
+            Console.ReadKey();
+        }
+    }
+}
